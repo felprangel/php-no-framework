1921cebd5caa0c10f8a7108d3279ceeef0086fe3
felprangel <feliperangel897@gmail.com>
2025-03-09 10:47:10 -0300

    adiciona request handler

--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Laminas\Diactoros\ServerRequestFactory;
 use NoFramework\HelloWorld;
+use Relay\Relay;
 
 use function DI\create;
 
@@ -17,6 +19,11 @@ $containerBuilder->addDefinitions([
 ]);
 
 $container = $containerBuilder->build();
+
+$middlewareQueue = [];
+
+$requestHandler = new Relay($middlewareQueue);
+$requestHandler->handle(ServerRequestFactory::fromGlobals());
 
 $helloWorld = $container->get(HelloWorld::class);
 $helloWorld->announce();
