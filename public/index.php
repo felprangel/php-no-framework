7d224a8e5f7bbc48f94c39749f946a14f868cd33
felprangel <feliperangel897@gmail.com>
2025-03-09 10:32:43 -0300

    adiciona teste do autoload

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use NoFramework\HelloWorld;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-echo 'Hello, world!';
+$helloWorld = new HelloWorld();
+$helloWorld->announce();
---
+++ src/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoFramework;
+
+class HelloWorld
+{
+    public function announce(): void
+    {
+        echo "Hello autoload world!";
+    }
+}
