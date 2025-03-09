72b8d899d26cf65a1a3cfe08538ffae6457bb960
felprangel <feliperangel897@gmail.com>
2025-03-09 10:56:05 -0300

    adiciona dependencia no construtor e utilização da dependencia na hora de usar a função

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -6,9 +6,11 @@ namespace NoFramework;
 
 class HelloWorld
 {
+    public function __construct(private string $foo) {}
+
     public function __invoke(): void
     {
-        echo 'Hello, autoloaded world!';
+        echo "Hello, {$this->foo} world!";
         exit;
     }
 }
