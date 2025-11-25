e6c46e2cf295fe71cba9a42b82609eb2249eb36b
felprangel <feliperangel897@gmail.com>
2025-11-24 23:05:34 -0300

    adiciona ponto central na aplicação para lidar com requests e passa respostas para cada arquivo

---
+++ front.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+$map = [
+    '/hello' => __DIR__ . '/hello.php',
+    '/bye' => __DIR__ . '/bye.php'
+];
+
+$path = $request->getPathInfo();
+
+if (isset($map[$path])) {
+    require $map[$path];
+    $response->send();
+    return;
+}
+
+$response->setStatusCode(404);
+$response->setContent('Not Found');
+$response->send();
