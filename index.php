01f78f3ea7f699664de64d385a69e745660f5e03
felprangel <feliperangel897@gmail.com>
2025-11-24 22:57:08 -0300

    adiciona arquivo para reduzir o boilerplate

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$request = Request::createFromGlobals();
+require_once __DIR__ . '/init.php';
 
 $name = $request->query->get('name', 'World');
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
-
-$response->prepare($request);
+$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
 $response->send();
