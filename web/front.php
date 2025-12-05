472a955b5cdd6997674e5115bdb508ab61c42cda
felprangel <feliperangel897@gmail.com>
2025-12-04 21:54:05 -0300

    ajusta framekwork para renderizar o template dinamicamente

--- web/front.php
+++ web/front.php
@@ -8,6 +8,15 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
+function renderTemplate(Request $request): Response
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
 
@@ -17,16 +26,13 @@ $matcher = new UrlMatcher($routes, $context);
 
 try {
     $path = $request->getPathInfo();
-    extract($matcher->match($path), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+    $request->attributes->add($matcher->match($path));
 
-    $response = new Response(ob_get_clean());
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
     $response = new Response('An error occurred', 500);
 }
-
 
 $response->send();
