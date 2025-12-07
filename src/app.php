ba4dc4be3d1b97e11b5c5ce6931e17b98277f732
felprangel <feliperangel897@gmail.com>
2025-12-06 21:22:35 -0300

    adiciona funcionalidade de saber se é ano bissexto

--- src/app.php
+++ src/app.php
@@ -1,10 +1,28 @@
 <?php
 
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
-$routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World', '_controller' => 'renderTemplate']));
-$routes->add('bye', new Route('/bye'));
+function is_leap_year(?int $year = null): bool
+{
+    if (null === $year) {
+        $year = (int)date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function (Request $request): Response {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+]));
 
 return $routes;
