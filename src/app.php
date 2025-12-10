eb5e4f16c71fcf15e3d9e1e4237023959196f191
felprangel <feliperangel897@gmail.com>
2025-12-09 23:21:27 -0300

    adiciona lógica para resolver os controllers automáticamente

--- src/app.php
+++ src/app.php
@@ -1,28 +1,12 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use App\LeapYearController;
 use Symfony\Component\Routing;
-
-function is_leap_year(?int $year = null): bool
-{
-    if (null === $year) {
-        $year = (int)date('Y');
-    }
-
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
 
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
-    '_controller' => function (Request $request): Response {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
+    '_controller' => [LeapYearController::class, 'index']
 ]));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -4,18 +4,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
-
-function renderTemplate(Request $request): Response
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
@@ -24,11 +17,17 @@ $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
 try {
     $path = $request->getPathInfo();
     $request->attributes->add($matcher->match($path));
 
-    $response = call_user_func($request->attributes->get('_controller'), $request);
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
