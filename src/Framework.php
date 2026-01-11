5d7db6e2de06cda6e05674709c39b9e6a0443b26
felprangel <feliperangel897@gmail.com>
2026-01-11 20:00:17 -0300

    cria classe específica para decidir o controller

---
+++ src/Framework.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+class Framework
+{
+    public function __construct(
+        private UrlMatcher $matcher,
+        private ControllerResolver $controllerResolver,
+        private ArgumentResolver $argumentResolver
+    )
+    {
+    }
+
+    public function handle(Request $request): Response
+    {
+        $this->matcher->getContext()->fromRequest($request);
+
+        try {
+            $pathInfo = $request->getPathInfo();
+            $request->attributes->add($this->matcher->match($pathInfo));
+
+            $controller = $this->controllerResolver->getController($request);
+            $arguments = $this->argumentResolver->getArguments($request, $controller);
+
+            return call_user_func_array($controller, $arguments);
+        } catch (ResourceNotFoundException $exception) {
+            return new Response('Not Found', 404);
+        } catch (Exception $exception) {
+            return new Response('An error occurred', 500);
+        }
+    }
+}
--- web/front.php
+++ web/front.php
@@ -2,11 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Framework;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
@@ -14,24 +13,12 @@ $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
 
 $context = new RequestContext();
-$context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-try {
-    $path = $request->getPathInfo();
-    $request->attributes->add($matcher->match($path));
-
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();
